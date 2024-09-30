95ebf71352427aa69f3a45c1ec249fccfefb9335
TIFFANY <crick.tiffany@yahoo.com>
2024-09-30 14:53:07 -0400

    "creating response"

---
+++ migrations/20240627181512_create_pet_type_table.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+// YYYYMMDDHHMMSS_create_pet_type_table.js
+exports.up = function(knex) {
+  return knex.schema.createTable('pet_type', table => {
+    table.increments();
+    table.string('name', 250)
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('pet_type');
+};
